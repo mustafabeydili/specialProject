fb5a9e6f65490b60c9ffe8ee6dc2c6df7d087492
mrtcnn88 <i3meronak88@gmail.com>
2018-01-03 22:02:46 +0300

    Düzenleme.

--- src/main/java/com/cybersoft/specialProject/controller/SoruController.java
+++ src/main/java/com/cybersoft/specialProject/controller/SoruController.java
@@ -4,6 +4,8 @@ import com.cybersoft.specialProject.document.Soru;
 import com.cybersoft.specialProject.repository.SoruRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,19 +23,49 @@ public class SoruController {
     // ilk sayfa 0 dan başlıyor !
 
     @GetMapping(value = "/brans/{brans}/unite/{unite}/{sayfa}/{kayitsayisi}")
-    public List<Soru> getAllSoruByBransUnitePaging(@PathVariable("brans") String brans, @PathVariable("unite") String unite, @PathVariable("sayfa") int sayfa, @PathVariable("kayitsayisi") int kayitsayisi) {
+    public List<Soru> getAllSoruByBransUnitePaging(@PathVariable("brans") String brans,
+                                                   @PathVariable("unite") String unite,
+                                                   @PathVariable("sayfa") int sayfa,
+                                                   @PathVariable("kayitsayisi") int kayitsayisi) {
         List<Soru> findByBransAndUnite = soruRepository.findByBransAndUnite(brans, unite, new PageRequest(sayfa, kayitsayisi));
         return findByBransAndUnite;
     }
 
     @GetMapping(value = "/brans/{brans}/unite/{unite}/bolum/{bolum}/{sayfa}/{kayitsayisi}")
-    public List<Soru> getAllSoruByBransUniteBolumPaging(@PathVariable("brans") String brans, @PathVariable("unite") String unite, @PathVariable("bolum") String bolum, @PathVariable("sayfa") int sayfa, @PathVariable("kayitsayisi") int kayitsayisi) {
+    public List<Soru> getAllSoruByBransUniteBolumPaging(@PathVariable("brans") String brans,
+                                                        @PathVariable("unite") String unite,
+                                                        @PathVariable("bolum") String bolum,
+                                                        @PathVariable("sayfa") int sayfa,
+                                                        @PathVariable("kayitsayisi") int kayitsayisi) {
         List<Soru> findByBransAndUniteAndBolum = soruRepository.findByBransAndUniteAndBolum(brans, unite, bolum, new PageRequest(sayfa, kayitsayisi));
         return findByBransAndUniteAndBolum;
     }
 
-    @PostMapping("/soruekle")
-    public void insertSoru(@RequestBody Soru soru) {
-        soruRepository.insert(soru);
+    @GetMapping(value = "/brans/{brans}/unite/{unite}/bolum/{bolum}/soruTip/{soruTip}/{sayfa}/{kayitsayisi}")
+    public List<Soru> getAllSoruByBransUniteBolumSoruTipPaging(@PathVariable("brans") String brans,
+                                                               @PathVariable("unite") String unite,
+                                                               @PathVariable("bolum") String bolum,
+                                                               @PathVariable("soruTip") String soruTip,
+                                                               @PathVariable("sayfa") int sayfa,
+                                                               @PathVariable("kayitsayisi") int kayitsayisi) {
+        List<Soru> findByBransAndUniteAndBolumAndSoruTip = soruRepository.findByBransAndUniteAndBolumAndSoruTip(brans, unite, bolum, soruTip, new PageRequest(sayfa, kayitsayisi));
+        return findByBransAndUniteAndBolumAndSoruTip;
+    }
+
+    @GetMapping(value = "/brans/{brans}/unite/{unite}/bolum/{bolum}/soruTip/{soruTip}/zorlukSeviye/{zorlukSeviye}/{sayfa}/{kayitsayisi}")
+    public List<Soru> getAllSoruByBransUniteBolumSoruTipZorlukSeviyePaging(@PathVariable("brans") String brans,
+                                                                           @PathVariable("unite") String unite,
+                                                                           @PathVariable("bolum") String bolum,
+                                                                           @PathVariable("soruTip") String soruTip,
+                                                                           @PathVariable("zorlukSeviye") String zorlukSeviye,
+                                                                           @PathVariable("sayfa") int sayfa,
+                                                                           @PathVariable("kayitsayisi") int kayitsayisi) {
+        List<Soru> findByBransAndUniteAndBolumAndSoruTipAndZorlukSeviye = soruRepository.findByBransAndUniteAndBolumAndSoruTipAndZorlukSeviye(brans, unite, bolum, soruTip, zorlukSeviye, new PageRequest(sayfa, kayitsayisi));
+        return findByBransAndUniteAndBolumAndSoruTipAndZorlukSeviye;
+    }
+
+    @PostMapping(value = "/soruekle")
+    public Soru insertSoru(@RequestBody Soru soru) {
+        return soruRepository.insert(soru);
     }
 }
--- src/main/java/com/cybersoft/specialProject/repository/SoruRepository.java
+++ src/main/java/com/cybersoft/specialProject/repository/SoruRepository.java
@@ -16,5 +16,4 @@ public interface SoruRepository extends MongoRepository<Soru, String> {
     List<Soru> findByBransAndUniteAndBolum(String brans, String unite, String bolum, Pageable pageable);
     List<Soru> findByBransAndUniteAndBolumAndSoruTip(String brans, String unite, String bolum, String soruTip, Pageable pageable);
     List<Soru> findByBransAndUniteAndBolumAndSoruTipAndZorlukSeviye(String brans, String unite, String bolum, String soruTip, String zorlukSeviye, Pageable pageable);
-    void insertSoru(Soru soru);
 }
